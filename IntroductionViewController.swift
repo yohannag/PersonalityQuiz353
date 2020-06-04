729e9f961761b1429fed4f6dc394670a52b33618
yohannag <47547553+yohannag@users.noreply.github.com>
2020-06-04 10:39:40 -0700

    Create IntroductionViewController.swift

---
+++ IntroductionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Yohanna Gebregiorgis on 5/19/20.
+//  Copyright © 2020 Yohanna Gebregiorgis. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
